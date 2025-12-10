b3e06495a098096c3b4f754080504e7ea11d522b
Corey Magin <thefancypants21@gmail.com>
2025-12-10 11:58:37 -0800

    Updated tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,6 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class',
-  safelist: [
-    {
-      pattern: /^(from|to|via)-(slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(50|100|200|300|400|500|600|700|800|900|950)$/,
-      variants: ['hover', 'group-hover'],
-    },
-  ],
   theme: {
     extend: {
       colors: {
